ca715ea263fbf78663918876ea3199d6b0e9f851
onesis <Onesis.olea@gmail.com>
2021-12-17 02:59:18 -0400

    basic Splash screen

---
+++ app/controllers/splash_screen_controller.rb
@@ -0,0 +1,5 @@
+class SplashScreenController < ApplicationController
+
+  def show;
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'splash_screen#show'
   devise_for :users
-  resources :entities
-  resources :groups
+  resources :users do
+    resources :entities
+    resources :groups
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
