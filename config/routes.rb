4eab14641694cd6a283f461d9d12be430ad3d83b
onesis <Onesis.olea@gmail.com>
2021-12-20 12:36:46 -0400

    basic groups views

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root to: 'splash_screen#show'
   devise_for :users
   resources :users do
-    resources :entities
-    resources :groups
+    resources :groups do
+      resources :entities
+   end   
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_16_234001) do
+ActiveRecord::Schema.define(version: 2021_12_20_144755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2021_12_16_234001) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_entities_on_user_id"
+  end
+
+  create_table "entities_groups", id: false, force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "entity_id", null: false
+    t.index ["group_id", "entity_id"], name: "index_entities_groups_on_group_id_and_entity_id"
   end
 
   create_table "groups", force: :cascade do |t|
